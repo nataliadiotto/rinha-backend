07f8449ad26b4795b3c672f3519a9765d8c12bdd
Natália Diotto <diottonatalia@Natalias-MacBook-Air.local>
2025-07-15 10:48:15 -0300

    Initial commit of Rinha Backend project

---
+++ src/main/java/com/rinhabackend/RinhaBackendApplication.java
@@ -0,0 +1,14 @@
+package com.rinhabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinhaBackendApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/main/java/com/rinhabackend/dto/PaymentRequest.java
@@ -0,0 +1,16 @@
+package com.rinhabackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentRequest {
+    private String correlationId;
+    private BigDecimal amount;
+
+}
