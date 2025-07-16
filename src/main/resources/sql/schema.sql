005618e341c91e056b62acb81d7c34815fe38ba7
nataliadiotto <diottonatalia@gmail.com>
2025-07-16 11:04:40 -0300

    create DDL

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE payments (
+    id SERIAL PRIMARY KEY,
+    correlation_id VARCHAR(36) UNIQUE NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    processor_type VARCHAR(10) NOT NULL,
+    processed_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
