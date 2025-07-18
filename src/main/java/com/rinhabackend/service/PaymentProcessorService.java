b4b3f4b373784c8d2b276cbeaef88deb53d23463
nataliadiotto <diottonatalia@gmail.com>
2025-07-18 15:53:30 -0300

    feat: scheduled health checks

--- src/main/java/com/rinhabackend/service/PaymentProcessorService.java
+++ src/main/java/com/rinhabackend/service/PaymentProcessorService.java
@@ -6,6 +6,7 @@ import com.rinhabackend.dto.HealthCheckResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -18,11 +19,25 @@ public class PaymentProcessorService {
 
     private final WebClient webClientDefault;
     private final WebClient webClientFallback;
+    private volatile HealthCheckResponse defaultHealthCache;
+    private volatile HealthCheckResponse fallbackHealthCache;
+    private volatile Instant lastDefaultCheckTime;
+    private volatile Instant lastFallbackCheckTime;
+
 
     @Autowired
     public PaymentProcessorService(WebClient.Builder webClientBuilder) {
-        this.webClientDefault = webClientBuilder.baseUrl("http://payment-processor-default:8080").build();
-        this.webClientFallback = webClientBuilder.baseUrl("http://payment-processor-fallback:8080").build();
+        //this.webClientDefault = webClientBuilder.baseUrl("http://payment-processor-default:8080").build();
+        //this.webClientFallback = webClientBuilder.baseUrl("http://payment-processor-fallback:8080").build();
+
+        this.webClientDefault = webClientBuilder.baseUrl("http://localhost:8001").build();
+        this.webClientFallback = webClientBuilder.baseUrl("http://localhost:8002").build();
+
+        this.defaultHealthCache = new HealthCheckResponse(true, Integer.MAX_VALUE);
+        this.fallbackHealthCache = new HealthCheckResponse(true, Integer.MAX_VALUE);
+        this.lastDefaultCheckTime = Instant.MIN;
+        this.lastFallbackCheckTime = Instant.MIN;
+
     }
 
     public Mono<ExternalPaymentResponse> processPayment(String processorType, String correlationId, BigDecimal amount, Instant requestedAt) {
@@ -81,6 +96,40 @@ public class PaymentProcessorService {
                 })
                 .bodyToMono(HealthCheckResponse.class); // Convert the response body to HealthCheckResponse Mono
     }
+
+    @Scheduled(fixedRate = 5000)
+    public void updateHealthCaches() {
+        System.out.println("Scheduled health check initiated.");
+
+        getProcessorHealth("DEFAULT")
+                .subscribe(
+                        health -> {
+                            defaultHealthCache = health;
+                            lastDefaultCheckTime = Instant.now();
+                            System.out.println("DEFAULT Health Updated: " + health);
+                        },
+                        error -> {
+                            defaultHealthCache = new HealthCheckResponse(true, Integer.MAX_VALUE);
+                            lastDefaultCheckTime = Instant.now();
+                            System.err.println("Error checking DEFAULT health: " + error.getMessage());
+                        });
+
+        getProcessorHealth("FALLBACK")
+                .subscribe(
+                        health -> {
+                            defaultHealthCache = health;
+                            lastFallbackCheckTime = Instant.now();
+                            System.out.println("FALLBACK Health Updated: " + health);
+                        },
+                        error -> {
+                            defaultHealthCache = new HealthCheckResponse(true, Integer.MAX_VALUE);
+                            lastFallbackCheckTime = Instant.now();
+                            System.err.println("Error checking FALLBACK Health: " + error.getMessage());
+                        });
+
+    }
+
+
 
 
 }
